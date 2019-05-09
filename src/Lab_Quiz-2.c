9b673dff7a2ad7cb474641bdb0170840d8d3166e
quarr <quarr@SnorgleLaptop3.newcastle.edu.au>
2019-05-09 18:30:28 +1000

    End of Quiz #2

--- src/Lab_Quiz-2.c
+++ src/Lab_Quiz-2.c
@@ -12,37 +12,53 @@
 #include <stdlib.h>
 #include <limits.h>
 
+void pixelMean(float *r, float *g, float *b);
+
+
 
 int main() {
 
 	float image[5][5][3];
-	int x, y, z;
+	int x, y;
+
+	FILE *inFile;
 
 	// Don’t change this srand() line!
-	srand(0);
+	//srand(0);
+
+	inFile = fopen("text_file.txt" , "r");
+
 
 	for(y = 0; y < 5; y++) {
 		for(x = 0; x < 5; x++) {
-			image[x][y][0] = (float)rand()/(float)INT_MAX;
-			image[x][y][1] = (float)rand()/(float)INT_MAX;
-			image[x][y][2] = (float)rand()/(float)INT_MAX;
+
+			fscanf(inFile,"%f %f %f", &image[x][y][0], &image[x][y][1], &image[x][y][2]);
+			printf("%d: %f %f %f \n", x, image[x][y][0],image[x][y][1],image[x][y][2]);
 		}
 	}
-
-	printf("Image Array is:");
-			for (y=0; y<5; y++){
-				for (x=0; x<5; x++){
-					printf("\n");
-					for (z=0; z<3; z++){
-						printf("(%d:%d:%d)", x, y, z);
-						printf("%f,", image[x][y][z]);
-
-					}
-				}
-			}
-			printf("\n\n");
+	fclose(inFile);
+	printf("\n\nMean\n");
 	// ----------------------------------------
 	// Write your code below this comment block
 	// ----------------------------------------
+		for(y = 0; y < 2; y++) {
+			for(x = 0; x < 5; x++) {
+				pixelMean(&image[x][y][0], &image[x][y][1],&image[x][y][2]);
+				printf("%d %d: %f %f %f \n", x , y, image[x][y][0],image[x][y][1],image[x][y][2]);
+				}
+		}
+
+
 	return 0;
+}
+
+void pixelMean(float *r, float *g, float *b){
+
+	float pxM;
+
+	pxM= ((*r)+(*g)+(*b))/3;
+	*r=pxM;
+	*g=pxM;
+	*b=pxM;
+	return ;
 }
